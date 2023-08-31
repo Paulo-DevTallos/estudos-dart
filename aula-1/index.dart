a302d2b5f318a6aed623f9457434bb7acf84d186
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-08-31 19:14:08 -0300

    adicionando novas etapas de estudo

--- aula-1/index.dart
+++ aula-1/index.dart
@@ -1,6 +1,6 @@
 //função main - todo o código em Dart é executado dentro da função main
-void main() {  
-
+// Para executar um arquivo .dart "dart run meu-path ou ."
+void main() {
   // variáveis - (Int, Double)
   // int - uma variável criada sem valor significa que seu valor é null, o Dart não permite criar variaveis vazias.
   int age = 32;
@@ -11,7 +11,62 @@ void main() {
   // booleanos - (true, false)
   bool heightIsEqualAge = (age == height); // essa opção retorna false
 
+  // tipos string
+  /**
+   * Para manipular Strings em frases maiores você pode utilizar a abordagem de aspa simples
+   * ex: 
+   * 'Aqui eu tenho uma frase $variavel'
+   * 'aqui é o complemento da frase.'
+   * 
+   * ou 
+   * 'Aqui eu tenho uma frase $variavel \n'
+   * 'aqui é o complemento da frase. \n'
+   * 
+   */
+  String nome = 'Meu nome é Paulo Sergio';
+  String name = 'Paulo Sergio';
+  String lastName = 'Santos Nascimento';
+
+  // manipulando listas - Lista tipo unico tipo list que recebe strings
+  List<String> namesList = ['name1', 'name2', 'name3', 'name4', 'name5'];
+  int countNames = namesList.length;
+
+  // lista dinamicas - permite que voce crie listas com varios tipos diferentes utilizar <dynamic>
+  // pode não ser tão performatico para o computador a depender do caso em que se use
+  // é necessário criar tipos de acordo com a necessidade de uso.
+  List<dynamic> paulo = [32, true, 'Paulo Sergio'];
+
+  // para manipular uma lista dentro de uma string
+  String phraseName = 'Meu nome é ${paulo[2]}';
+  String phraseAge = 'Minha idade é ${paulo[0]}';
+
+  // manipulando dados em listas com variaveis
+  int myAge = 32;
+  double myHeight = 1.67;
+  // a variavel const não permite a alteração de dados em uma variavel
+  // seguindo a mesma primicia de final que tambem não permite alteração.
+  const String myName = 'Paulo Sergio';
+  String teste = 'Só um teste usando const ${myName = 'teste'}';
+  print(teste);
+
+  List<dynamic> aboutMe = [myAge, myHeight, myName];
+
+  print(myAge);
+  // alterando dados referente a variável myAge, nesse caso ele mantem a original alterando só a referencia
+  aboutMe[0] = 35;
+  print(aboutMe[0]);
+
   print(age);
   print(height);
   print(heightIsEqualAge);
+  print(nome);
+  print(name + ' ' + lastName);
+  print('Meu nome completo é $name $lastName');
+
+  print(namesList);
+  print(countNames);
+  print(paulo);
+  print(phraseName);
+  print(phraseAge);
+  print(aboutMe);
 }
